922aa730d46788f0215b54c8ff68e2b506939940
jjamin <c1jang@hanmail.net>
2022-07-04 21:09:46 +0900

    20220704

--- react_01/src/App.js
+++ react_01/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
+  const leevar = <h1>이창훈</h1>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      {leevar}
+      <div>hhhdsdsdsdsdxdsdsdsdghghg</div>
+      <Header />
+    </>
+
   );
 }
 
---
+++ react_01/src/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+  const leevar = <h2>나는 이창후일까? 아닐까?</h2>;
+  return (
+    <>
+      {leevar}
+      <div>hahahahaha</div>
+    </>
+
+  );
+}
+
+export default Header;
