a137372f69ea7c0be56773be61d7342909f94e07
Your Namewngmlchoi@naver.comwngmlchoi@naver.co <wngmlchoi@naver.com>
2022-07-25 12:21:57 +0900

    20220725

---
+++ react00/src/App.js
@@ -0,0 +1,20 @@
+import Test from "./test";
+import Nav from "./Nav";
+
+const App = () => {
+  // console.log('하하하')
+  return (
+    <>
+
+      <Test />
+      <h1>호호호</h1>
+      <Nav />
+      <Test />
+      <Test />
+      {/* <h2>하하하</h2> */}
+    </>
+  )
+
+
+}
+export default App;
