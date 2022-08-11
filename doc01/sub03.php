cb030d679bd549346f1c228379ec09cabd612e7b
Your Namewngmlchoi@naver.comwngmlchoi@naver.co <wngmlchoi@naver.com>
2022-08-11 15:26:12 +0900

    20220811

---
+++ doc01/sub03.php
@@ -0,0 +1,7 @@
+<? include 'head.php'?>
+<? $num = 3;
+?>
+<article>
+            나는 서버03
+        </article>
+        <? include 'tail.php'?>
---
+++ project/sub01.php
@@ -0,0 +1,38 @@
+<!-- 헤드가 들어오기 전에 넣어줘야함 -->
+<?
+$subnum = 1;
+$pagenum = 1;
+$sub_title = 'ceo 인사말';
+$page_title = '기업소개';
+?>
+<? include 'head.php' ?>
+<div class="content">
+
+    <p>
+        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+    </p>
+    <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta sed in iure laborum sequi itaque.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta sed in iure laborum sequi itaque.
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet.
+    </p>
+    <table class="table01">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>구분</th>
+                <th>가격</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>오뎅</td>
+                <td>1억원</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+<? include 'tail.php' ?>
