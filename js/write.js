49d49a7867f2b6238ac7199e5cf82c27b9f2087f
juhee <wngmlchoi@naver.com>
2022-10-09 23:53:39 +0900

    1009

--- js/write.js
+++ js/write.js
@@ -55,19 +55,14 @@ function sample4_execDaumPostcode() {
 }
 
 // 이미지 업로드
-$("#ex_file").on("change", function () {
-  ext = $(this).val().split(".").pop().toLowerCase(); //확장자
-  //배열에 추출한 확장자가 존재하는지 체크
-  if ($.inArray(ext, ["gif", "png", "jpg", "jpeg"]) == -1) {
-    resetFormElement($(this)); //폼 초기화
-    window.alert(
-      "이미지 파일이 아닙니다! (gif, png, jpg, jpeg 만 업로드 가능)"
-    );
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      document.getElementById("preview").src = e.target.result;
+    };
+    reader.readAsDataURL(input.files[0]);
   } else {
-    file = $("#ex_file").prop("files")[0];
-    blobURL = window.URL.createObjectURL(file);
-    $("#image_preview img").attr("src", blobURL);
-    $("#image_preview").slideDown(); //업로드한 이미지 미리보기
-    $(this).slideUp(); //파일 양식 감춤
+    document.getElementById("preview").src = "";
   }
-});
+}
