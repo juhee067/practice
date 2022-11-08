017c0a5b66ebf68f7be39c78b3a4c1289554d145
Your Namewngmlchoi@naver.comwngmlchoi@naver.co <wngmlchoi@naver.com>
2022-11-08 18:09:47 +0900

    20221108

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  let post = '강남 우동 맛집'
   return (
     <div className="App">
-
+      <div className='blackNav'>
+        <h4 style={{ color: 'red' }}>블로그임</h4>
+      </div>
+      <h4>{post}</h4>
+      {/* 데이터를 꽂아넣는다 : 데이터 바인딩 */}
     </div>
   );
 }
