a382a9829e1843848573be1aa44414b484b3480c
Your Namewngmlchoi@naver.comwngmlchoi@naver.co <wngmlchoi@naver.com>
2022-11-09 18:13:16 +0900

    20221109

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -1,8 +1,15 @@
 
 import './App.css';
-
+import { useState } from "react";
 function App() {
   let post = '강남 우동 맛집'
+  // 1. import {useState}
+  // 2. useState('보관할 자료') 
+  // 3. let [작명, 작명(state 변경도와주는 함수)]
+  // state는 변동시 자동으로 html에 반영되게 만들고 싶을때 사용
+  let [title, b] = useState('남자코트추천')
+  // destructuring표현
+  let [a, c] = [1, 2]
   return (
     <div className="App">
       <div className='blackNav'>
@@ -10,6 +17,10 @@ function App() {
       </div>
       <h4>{post}</h4>
       {/* 데이터를 꽂아넣는다 : 데이터 바인딩 */}
+      <div className="list">
+        <h4>{title}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
